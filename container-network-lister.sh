5138b840c9ecba9ae4407dd3802d2091a2d643e8
denny <denny.lai@farobottech.com>
2024-10-02 21:43:46 +0800

    improve readability by here-doc (thanks to ChatGPT)

--- container-network-lister.sh
+++ container-network-lister.sh
@@ -12,10 +12,26 @@ do
     printf "[port_mapping]\n$port_mapping\n\n"
     printf "[network]\n"
 
-    python3 -c "import json; result=[]; \
-                network_json=json.loads('$network_setting_json'); \
-                result=[{'Name': net_name, 'ID': network_json[net_name]['NetworkID'][:12], 'IP': ('{}/{}').format(network_json[net_name]['IPAddress'], network_json[net_name]['IPPrefixLen'])} for net_name in network_json]; \
-                print(json.dumps(result, indent=4));"
+    # here-document (a way to pass multiline strings to commands), <<EOF ~ EOF are passed to python
+    python3 - <<EOF
+import json
+result=[]
+network_json = json.loads('''$network_setting_json''')
+for net_name, details in network_json.items():
+
+    result.append({
+        "Name": net_name,
+        "ID": details["NetworkID"][:12],
+        "IP": f'{details["IPAddress"]}/{details["IPPrefixLen"]}'
+    })
+
+print(json.dumps(result, indent=4))
+EOF
+
+    # python3 -c "import json; result=[]; \
+    #             network_json=json.loads('$network_setting_json'); \
+    #             result=[{'Name': net_name, 'ID': network_json[net_name]['NetworkID'][:12], 'IP': ('{}/{}').format(network_json[net_name]['IPAddress'], network_json[net_name]['IPPrefixLen'])} for net_name in network_json]; \
+    #             print(json.dumps(result, indent=4));"
 
     echo "---------------------------"
 done
